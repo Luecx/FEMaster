5a27e6fefd0050b0208d8c11e0a85aff72f2ae56
Finn Eggers <web@finneggers.de>
2026-08-21 16:12:11 +0200

    bump version to 2.7.0

--- src/core/version.h
+++ src/core/version.h
@@ -29,13 +29,13 @@ namespace fem {
 *
 * This number increments when backward-compatible functionality is added.
 */
-#define VERSION_MINOR 6
+#define VERSION_MINOR 7
 
 /**
 * @brief The patch version number for the FEMaster solver.
 *
 * This number increments for backward-compatible bug fixes.
 */
-#define VERSION_PATCH 34
+#define VERSION_PATCH 0
 } // namespace fem
 
