e0eeb7152dc3d580b0f01f935ad62d643926326d
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-08 17:04:49 +0200

    Register solver keyword for Abaqus input

--- src/io/reader/parser_abq.cpp
+++ src/io/reader/parser_abq.cpp
@@ -100,6 +100,7 @@ void ParserAbq::register_common_commands(io::dsl::Registry& registry) {
     commands::register_spring(registry, model());
     commands::register_equation(registry, model());
     commands::register_coupling(registry, model());
+    commands::register_loadcase_solver(registry, *this);
 
     commands_abq::register_expansion(registry, model());
     commands_abq::register_orientation(registry, model());
