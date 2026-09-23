3a8b1e35ccc8153c5a6a20a5c2a9a179a6fba894
Finn Eggers <34272583+Luecx@users.noreply.github.com>
2026-09-23 19:02:55 +0200

    Move STEP controls and procedure registration into shared reader

--- src/io/reader/parser_abq.h
+++ src/io/reader/parser_abq.h
@@ -31,25 +31,10 @@ namespace fem::io::reader {
 /**
  * @brief Runtime state required while translating one supported Abaqus step.
  */
-struct ParserAbqState {
-    std::unordered_map<ID, std::string> node_transforms;
-
-    bool        step_seen      = false;
-    bool        step_active    = false;
-    int         max_increments = 100;
-    bool        nlgeom         = false;
-    bool        perturbation   = false;
-    Precision   step_period    = Precision(1);
-    std::string step_amplitude;
-};
-
 /**
  * @brief Abaqus dialect specialization of the common parsed-deck reader.
  */
 class ParserAbq : public Parser {
-private:
-    ParserAbqState m_abq_state;
-
 public:
     ParserAbq();
     ~ParserAbq() override = default;
@@ -57,7 +42,6 @@ public:
     ParserAbqState& abaqus_state();
     const ParserAbqState& abaqus_state() const;
 
-    std::pair<Precision, std::string> resolve_load_amplitude(const std::string& amplitude);
 
 protected:
     void register_commands(io::dsl::Registry& registry) override;
